b98d9157df5ecc00038406a3f6311d077eb94646
mme00w <53547569+mme00w@users.noreply.github.com>
2019-10-30 19:54:45 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,103 @@
+// スクロール量を取得する要素を取得
+var scrollElm = (function() {
+  if('scrollingElement' in document) {
+    return document.scrollingElement;
+  }
+  if(navigator.userAgent.indexOf('WebKit') != -1) {
+    return document.body;
+  }
+  return document.documentElement;
+})();
+
+// 全てのセクション要素を取得
+var sections = document.querySelectorAll('.section');
+
+// 全体をz方向に動かす#scaler要素を取得
+var scaler = document.getElementById('scaler');
+
+// 画面の高さを設定する#scroll要素を取得
+var scrollDiv = document.getElementById('scroll');
+
+var base = document.getElementById("base");
+
+// var getZ = window.getComputedStyle(scaler, null).getPropertyValue('transform');
+// console.log(getZ);
+
+var getZ = window.getComputedStyle(scaler, null);
+console.log(getZ);
+
+
+// var props2 = $('##scale').css('transform');
+ 
+// function transform3d_value(e){
+//   var values = e.split('(')[1];
+//   values = values.split(')')[0];
+//   values = values.split(', ');
+//   var matrix = {
+//     'scale-x':values[0],
+//     'rotate-z-p':values[1],
+//     'rotate-y-p':values[2],
+//     'perspective1':values[3],
+//     'rotate-z-m':values[4],
+//     'scale-y':values[5],
+//     'rotate-x-p':values[6],
+//     'perspective2':values[7],
+//     'rotate-y-m':values[8],
+//     'rotate-x-m':values[9],
+//     'scale-z':values[10],
+//     'perspective3':values[11],
+//     'translate-x':values[12],
+//     'translate-y':values[13],
+//     'translate-z':values[14],
+//     'perspective4':values[15]
+//   };
+//   return matrix;
+// }
+// console.log(transform3d_value(props2));
+
+// let re_mat3d = /^matrix3d\((.*)\)$/, m = matrix3d.match(re_mat3d)
+// ;
+// if( m ) {
+//   // m を行列（Array）に矯正
+//   m = m[1].split(/,\s*/).map( n => Number(n) )
+//   console.log( m ) // 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 3.88, 1
+// }
+
+// getZ.transform[226]でトランスフォーム
+
+// transform: "matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 3.88, 1)"
+
+
+// transform: "matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 2.54, 1)"
+
+// セクション要素のdata-z属性を取得し、transformを設定
+// 最後のセクション要素のdata-zを元に、画面の高さを計算して設定
+for(var i = 0; sections.length > i; i++) {
+  var itemZ = sections[i].getAttribute('data-z');
+  var itemX = sections[i].getAttribute('data-x');
+  var itemY = sections[i].getAttribute('data-y');
+  sections[i].style.transform = 'translateZ(' + - itemZ + 'px)';
+  base.style.perspectiveOrigin = itemX + '% ' + itemZ + '%';
+  if(i === sections.length -1) {
+    scrollDiv.style.height = itemZ + window.innerHeight + 'px';
+    
+  }
+}
+
+// for(var i = 0; sections.length > i; i++) {
+//   var itemZ = sections[i].getAttribute('data-z');
+//   var itemX = sections[i].getAttribute('data-x');
+//   var itemY = sections[i].getAttribute('data-y');
+//   sections[i].style.transform = 'translateZ(' + - itemZ + 'px)';
+//   base.style.perspectiveOrigin = itemX + '% ' + itemZ + '%';
+//   if(i === sections.length -1) {
+//     scrollDiv.style.height = itemZ + window.innerHeight + 'px';
+    
+//   }
+// }
+
+
+// スクロールイベントで、#scaler要素のtransformでz軸を動かす
+window.addEventListener('scroll', function() {
+  scaler.style.transform = 'translateZ(' + scrollElm.scrollTop / 100 + 'px)';
+});
