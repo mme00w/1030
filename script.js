e2d13ef8d1ed6be95ed6ea0371c9342d326f4417
enku <enku.aluco@outlook.jp>
2019-11-06 17:43:30 +0900

    no message

--- script.js
+++ script.js
@@ -91,13 +91,14 @@ var Zscroll=matrix3dToArray($('#scaler').css('transform'));
 
 console.log(Zscroll);
 
-  if(Zscroll >= 2 && Zscroll <= 3){
+  if(Zscroll >= 2 && Zscroll <= 3){ //Z値が２以上３以下の場合
 
-    var section02x = $('.section-2').getAttribute('data-x')
+    var section02x = $('.section-2').getAttribute('data-x') //section-2のdata-xを取得
 
-    var section02y = $('.section-2').getAttribute('data-y')
+    var section02y = $('.section-2').getAttribute('data-y') //section-2のdata-yを取得
     
-    base.style.perspectiveOrigin = section02x + '% ' + section02y + '%';
+    base.style.perspectiveOrigin = section02x + '% ' + section02y + '%'; 
+    //baseのスタイルにperspective-originを、値にsection02xとsection02yを代入
 
 }
 
